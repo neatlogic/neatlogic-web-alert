3d5d13181e439c03c2b7f80f82d27ab57cf4f70e
chenqw <gdnpc@163.com>
2025-07-21 14:26:48 +0800

    adjust

--- pages/leftmenu/alert-menu.vue
+++ pages/leftmenu/alert-menu.vue
@@ -96,7 +96,7 @@ export default {
   data: function() {
     return {
       alertCatalogList: [],
-      searchParam: { isActive: 1, needView: 1, pageSize: 10, currentPage: 1 },
+      searchParam: { isActive: 1, needView: 1, pageSize: 20, currentPage: 1 },
       pageCount: 0,
       isViewEdit: false,
       isCatalogEdit: false,
