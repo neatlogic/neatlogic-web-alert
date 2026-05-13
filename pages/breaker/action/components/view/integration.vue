03ce5714156039f90f537e8eeeb351584607dd66
chenqw <gdnpc@163.com>
2026-05-13 10:04:09 +0800

    adjust

--- pages/breaker/action/components/view/integration.vue
+++ pages/breaker/action/components/view/integration.vue
@@ -3,7 +3,12 @@
     <TsFormItem label="集成" labelPosition="left" style="margin:0px !important;">
       <span>{{ config.integrationUuid || '-' }}</span>
     </TsFormItem>
-    <TsFormItem v-if="config.paramMapping && config.paramMapping.length > 0" label="参数映射" labelPosition="left" style="margin:0px !important;">
+    <TsFormItem
+      v-if="config.paramMapping && config.paramMapping.length > 0"
+      label="参数映射"
+      labelPosition="left"
+      style="margin:0px !important;"
+    >
       <div>
         <div v-for="(item, index) in config.paramMapping" :key="index">
           <span class="text-grey">{{ item.name }}：</span>
